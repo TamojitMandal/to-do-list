c8259a651cbb9e23a5c134570e328ff90cc27eb9
Tamojit Mandal <tamojitm23@gmail.com>
2024-08-08 11:18:54 +0530

    TODO_LIST

---
+++ to-do-list/src/components/EditTodoForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    console.log('Updating todo:', value, task.id);
+    // edit todo
+    editTodo(value, task.id);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder='Update task'
+      />
+      <button type="submit" className='todo-btn'>Update Task</button>
+    </form>
+  );
+};
+
+export default EditTodoForm;
